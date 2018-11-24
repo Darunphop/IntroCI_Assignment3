09ce3e4c701109d350abe6b5a194f705aa854a23
darunphop <darunphop.p@gmail.com>
2018-11-24 16:48:46 +0700

    imported MLP.py

---
+++ MLP.py
@@ -0,0 +1,31 @@
+import activation as act
+import numpy as np
+
+def modelInit(model):
+    layerSize = [int(n[:-1]) for n in model.split('-')]
+    activationLayer = [n[-1:] for n in model.split('-')]
+    nHidden = len(layerSize) - 1
+
+    weight = []
+    for i in range(nHidden):
+        weight.append(np.random.randn(layerSize[i+1], layerSize[i]))
+        # weight.append(np.ones((layerSize[i+1], layerSize[i])) / 5**-2)
+    bias = []
+    for i in range(nHidden):
+        bias.append(np.random.rand(layerSize[i+1]))
+
+    return weight, bias, activationLayer
+
+def feedForward(input, weigth, bias, activation):
+    res = []
+    tmp = []
+    
+
+    for i in range(len(activation)-1):
+        tmp = np.dot(len(tmp)==0 and input or tmp, np.transpose(weigth[i]))
+        for j in range(tmp.shape[0]):
+            for k in range(tmp.shape[1]):
+                tmp[j][k] += bias[i][k]
+        tmp = act.activate(np.copy(tmp), activation[i+1])
+        res.append(np.asarray(tmp))
+    return res
