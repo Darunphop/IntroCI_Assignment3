6b91b5c48e876b67375881e797a3191e3d467f24
darunphop <darunphop.p@gmail.com>
2018-12-09 13:15:00 +0700

    use gc

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import preprocess as pp
 import GA as ga
 import MLP
 import numpy as np
+import gc
 
 def test(x):
     return x + 1000
@@ -21,13 +22,16 @@ if __name__ == '__main__':
     # print(testSet[0][:,0])
     # print(trainSet[0][:,0])
     # MLP.getError(o[-1],trainSet[0][:,0])
-    gai = ga.GeneticAlgorithm((MLP.getError, MLP.feedForward), MLP.modelInit, model, 100, 1, 0.2, 0)
+    gai = ga.GeneticAlgorithm((MLP.getError, MLP.feedForward), MLP.modelInit, model, 100, 1, 0.2, 0.1)
     # print(gai.fitness)
     gai.updateFitness((trainSet[0][:,1:],trainSet[0][:,0]))
     # print(gai.fitness)
+    print(len(gai.population))
     gai.selection()
+    print(len(gai.population))
     # print(gai.population[0][0])
     # print(gai.population[1][0])
     # del gai.population
     # print(gai.population)
     # print(gai.getFitness(test))
+    gc.collect()
